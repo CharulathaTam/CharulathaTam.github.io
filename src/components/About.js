05532004ed05e6425dc51379fe21254d23d70743
Charulatha <91403284+CharulathaTam@users.noreply.github.com>
2024-12-08 19:37:57 +0000

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <section id="about" className="about">
+      <h5>About me</h5>
+      <h6>Hello!</h6>
+      <h4>
+        I'm Charu
+      </h4>
+      <p>A problem solver at heart</p> 
+      <p>I'm diving into cybersecurity to combat digital threats and protect networks and infrastructures.</p>
+    </section>
+  );
+}
+
+export default About;
